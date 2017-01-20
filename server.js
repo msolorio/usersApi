3316e68ebafbaf9e481c8c10029bf31cedfdd57e
michael <msolorio789@gmail.com>
2017-01-19 22:49:07 -0800

    setup project

---
+++ server.js
@@ -0,0 +1,24 @@
+// get the packages we need
+const express = require('express');
+
+// create our application
+const app = express();
+
+// use environment defined port or 3000
+const port = process.env.PORT || 3000;
+
+// create our express router
+const router = express.Router();
+
+// initial dummy route for testing
+// http://localhost:3000/api
+router.get('/', (req, res) => {
+  res.json({message: 'a get request has been made to /api'});
+});
+
+// register all our routes with /api
+app.use('/api', router);
+
+// start the server
+app.listen(port);
+console.log(`server listening on port ${port}`);
