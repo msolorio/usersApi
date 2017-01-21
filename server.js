afd06ed671d9fa136c230179ac4d47576e3ced31
michael <msolorio789@gmail.com>
2017-01-21 11:09:21 -0800

    add functionality for GET single user PUT and DELETE

--- server.js
+++ server.js
@@ -72,6 +72,64 @@ allUsersRoute.get((req, res) => {
   });
 });
 
+// create an endpoint for /api/users/:user_id for GETs
+singleUserRoute.get((req, res) => {
+
+  // calling User model's findById method
+  // passing in id from URL to fetch a specific user
+  User.findById(req.params.user_id, (err, user) => {
+    if (err) res.send(err);
+
+    // send user as json in response
+    res.json(user);
+  });
+});
+
+// create an endpoint for /api/users/:user_id for PUTs
+// will update a user's data
+singleUserRoute.put((req, res) => {
+
+  // calling User model's findById method
+  // passing in user_id from URL to fetch a specific user
+  User.findById(req.params.user_id, (err, user) => {
+    if (err) res.send(err);
+
+    // update user's info with values passed in request
+    // body
+    user.username = req.body.username || user.username;
+    user.password = req.body.password || user.password;
+    user.firstname = req.body.firstname || user.firstname;
+    user.lastname = req.body.lastname || user.lastname;
+    user.address1 = req.body.address1 || user.address1;
+    user.address2 = req.body.address2 || user.address2;
+    user.city = req.body.city || user.city;
+    user.state = req.body.state || user.state;
+    user.zip = req.body.zip || user.zip;
+    user.age = req.body.age || user.age;
+
+    // call mongoose's save method on the user model
+    user.save((err) => {
+      if (err) res.send(err);
+
+      res.json(user);
+    })
+  })
+});
+
+// create an endpoint for /api/users/:user_id for DELETEs
+// passing in user_id from URL to fetch a specific user
+singleUserRoute.delete((req, res) => {
+
+  // calling User model's findByIdAndRemove method passing in
+  // the user_id from the URL
+  User.findByIdAndRemove(req.params.user_id, (err, user) => {
+    if (err) res.send(err);
+
+    // if there is no error send a message in the response
+    res.json({message: `user with id ${req.params.user_id} has been removed`});
+  });
+});
+
 // register all our routes with /api
 app.use('/api', router);
 
